ae8d8b41f5552ee3b119ed6071079de08cc2705e
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2022-01-26 18:07:39 +0300

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,23 +1,11 @@
-x=int(input('Введите число:'))
-A=[]
+def X_input()
+#Андрей
 
-def x2(x):
-    x /= 2
-    return collatz(x)
+def x2()
+#Сережа
 
-def x3_1(x):
-    x = 3*x+1
-    return collatz(x)
+def x3_1(x)
+#Ксюша
 
-def collatz (x):
-    if x==1:
-        A.append(int(x))
-        return x
-    elif x%2==0:
-        A.append(x)
-        return x2(x)
-    else:
-        A.append(x)
-        return x3_1(x)
-collatz(x)
-print(A)
+def collatz ():
+    
